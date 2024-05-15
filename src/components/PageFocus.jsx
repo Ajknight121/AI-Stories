b90c9ef66777209130695c942606e8d58d30aa54
Ajknight121 <97642775+Ajknight121@users.noreply.github.com>
2024-05-15 18:12:05 -0500

    initial interactivity for modes and text input

--- src/components/PageFocus.jsx
+++ src/components/PageFocus.jsx
@@ -1,14 +1,25 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
 
-export const PageFocus = ({page}) => {
-  let {title, prompt, order, image} = page
+export const PageFocus = ({ page }) => {
+  let { title, prompt, order, image } = page;
+  let [prompting, setPrompting] = useState("");
+  let [selected, setSelected] = useState(1);
   return (
     <div className="page-focus">
       <div className="title">{title}</div>
-      <input type="text" className="prompt">Enter prompt here</input>
+      <input
+        type="text"
+        value={prompting}
+        onChange={(e) => setPrompting(e.target.value)}
+        placeholder="Enter prompt here"
+      />
       <div className="page">
-        <div className="mode">Use Prompt or draw image</div>
+        <div className="mode">
+          <button className={`mode-button ${selected == 1 ? "selected" : ""}`} onClick={() => setSelected(1)}>Propmt AI</button>
+          <button className={`mode-button ${selected == 2 ? "selected" : ""}`} onClick={() => setSelected(2)}>Draw Image</button>
+        </div>
         <div className="canvas">
           <img src={image} width={"100%"} />
         </div>
